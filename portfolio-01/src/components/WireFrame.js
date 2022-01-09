8974a890117618c3171b3fc5038cc4619c020710
Burton Parker <burtonparker@gmail.com>
2022-01-08 22:31:56 -0600

    rough wireframe file just to start

---
+++ portfolio-01/src/components/WireFrame.js
@@ -0,0 +1,106 @@
+class ProductCategoryRow extends Component {
+    render() {
+      const category = this.props.category;
+      return (
+        <tr>
+          <th colSpan="2">
+            {category}
+          </th>
+        </tr>
+      );
+    }
+  }
+  
+  class ProductRow extends Component {
+    render() {
+      const product = this.props.product;
+      const name = product.stocked ?
+        product.name :
+        <span style={{color: 'red'}}>
+          {product.name}
+        </span>;
+  
+      return (
+        <tr>
+          <td>{name}</td>
+          <td>{product.price}</td>
+        </tr>
+      );
+    }
+  }
+  
+  class ProductTable extends Component {
+    render() {
+      const rows = [];
+      let lastCategory = null;
+      
+      this.props.products.forEach((product) => {
+        if (product.category !== lastCategory) {
+          rows.push(
+            <ProductCategoryRow
+              category={product.category}
+              key={product.category} />
+          );
+        }
+        rows.push(
+          <ProductRow
+            product={product}
+            key={product.name} />
+        );
+        lastCategory = product.category;
+      });
+  
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      );
+    }
+  }
+  
+  class SearchBar extends Component {
+    render() {
+      return (
+        <form>
+          <input type="text" placeholder="Search..." />
+          <p>
+            <input type="checkbox" />
+            {' '}
+            Only show products in stock
+          </p>
+        </form>
+      );
+    }
+  }
+  
+  class FilterableProductTable extends Component {
+    render() {
+      return (
+        <div>
+          <SearchBar />
+          <ProductTable products={this.props.products} />
+        </div>
+      );
+    }
+  }
+  
+  
+  const PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+  ];
+   
+  ReactDOM.render(
+    <FilterableProductTable products={PRODUCTS} />,
+    document.getElementById('container')
+  );
