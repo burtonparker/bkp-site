a2b57c6afebe839dac39043f9a3e7661997ff1e8
Burton Parker <burtonparker@gmail.com>
2022-01-11 17:06:18 -0600

    fluid width header

--- portfolio-01/src/components/HeaderComponent.js
+++ portfolio-01/src/components/HeaderComponent.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import { Jumbotron } from 'reactstrap';
 
 class Header extends Component {
     render() {
       return (
         <React.Fragment>
-                    <div className="container text-light bg-dark bg-gradient">
-                        <div className="row">
-                            <div className="col">
-                                <h1>Portfolio</h1>
-                                <h2>You can't love your team without hating another team.</h2>
-                            </div>
-                        </div>
+            <div className="container-fluid text-light bg-dark bg-gradient">
+                <div className="row">
+                    <div className="col">
+                        <h1>Portfolio</h1>
+                        <h2>You can't love your team without hating another team.</h2>
                     </div>
+                </div>
+            </div>
         </React.Fragment>
       );
     };
