d4302cf5f06b81939d7a5c9351b2c2ecfa4f1273
Burton Parker <burtonparker@gmail.com>
2022-01-14 11:55:59 -0600

    making skills colors map work, editing about page

--- portfolio-01/src/components/AboutComponent.js
+++ portfolio-01/src/components/AboutComponent.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 class About extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="col-md-8">
-                    <h1>
-                        About Me
-                    </h1>
-                </div>
-                <div className="col-md-4">
-                    <p>
-                    Elementum nisi quis eleifend quam. Arcu risus quis varius quam quisque id diam vel. Odio tempor orci dapibus ultrices in iaculis nunc sed. Id consectetur purus ut faucibus pulvinar elementum integer enim. Elementum curabitur vitae nunc sed velit dignissim sodales. Nisi est sit amet facilisis magna etiam tempor. Facilisi etiam dignissim diam quis enim. Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Gravida rutrum quisque non tellus orci ac. Diam volutpat commodo sed egestas egestas. In ante metus dictum at tempor commodo ullamcorper a lacus. Quisque egestas diam in arcu cursus. Erat imperdiet sed euismod nisi porta lorem. Aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros.
-                    </p>
+                <div className="container mt-3">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <h1>
+                                <Card>
+                                    <CardImg width="100%" img src="../assets/images/burton-karas-parker-about.jpg" alt="Burton Karas Parker" />
+                                </Card>
+                            </h1>
+                        </div>
+                        <div className="col-md-8">
+                            <p>
+                            Elementum nisi quis eleifend quam. Arcu risus quis varius quam quisque id diam vel. Odio tempor orci dapibus ultrices in iaculis nunc sed. Id consectetur purus ut faucibus pulvinar elementum integer enim. Elementum curabitur vitae nunc sed velit dignissim sodales. Nisi est sit amet facilisis magna etiam tempor. Facilisi etiam dignissim diam quis enim. Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Gravida rutrum quisque non tellus orci ac. Diam volutpat commodo sed egestas egestas. In ante metus dictum at tempor commodo ullamcorper a lacus. Quisque egestas diam in arcu cursus. Erat imperdiet sed euismod nisi porta lorem. Aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         );
--- portfolio-01/src/shared/skills.js
+++ portfolio-01/src/shared/skills.js
@@ -2,51 +2,56 @@ export const SKILLS = [
     {
         id: 0,
         color: "#00a8f4",
-        text: "• React"
+        text: " React"
     },
     {
         id: 1,
         color: "#e91e62",
-        text: "• React Native"
+        text: " React Native"
     },
     {
         id: 2,
         color: "#f44336",
-        text: "• Redux"
+        text: " Redux"
     },
     {
         id: 3,
         color: "#9c27b0",
-        text: "• Node JS"
+        text: " Node JS"
     },
     {
         id: 4,
         color: "#673ab6",
-        text: "• Express"
+        text: " Express"
     },
     {
         id: 5,
         color: "#8bc24a",
-        text: "• MongoDB"
+        text: " MongoDB"
     },
     {
         id: 6,
         color: "#009688",
-        text: "• Photoshop"
+        text: " Bootstrap"
     },
     {
         id: 7,
-        color: "#ff9800",
-        text: "• Illustrator"
+        color: "#cc3300",
+        text: " Photoshop"
     },
     {
         id: 8,
-        color: "#ffeb3a",
-        text: "• Lightroom"
+        color: "#ff9800",
+        text: " Illustrator"
     },
     {
         id: 9,
+        color: "#ffcc00",
+        text: " Lightroom"
+    },
+    {
+        id: 10,
         color: "#607d8a",
-        text: "• Microsoft/Google/Apple Business Suites"
+        text: " Microsoft/Google/Apple Business Suites"
     }
 ];
