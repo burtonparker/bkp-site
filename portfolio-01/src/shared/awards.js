de02d12837f7be3332ca8c0892cd162905fab59c
Burton Parker <burtonparker@gmail.com>
2022-02-12 12:52:44 -0600

    adding hidden value details to awards.js

--- portfolio-01/src/shared/awards.js
+++ portfolio-01/src/shared/awards.js
@@ -1,18 +1,22 @@
 export const AWARDS = [
     {
         id: 0,
-        text: "2012 Scion Musicless Music Conference Panelist"
+        text: "2012 Scion Musicless Music Conference Panelist",
+        details: "Chosen as one of only four merch company representatives to speak..."
     },
     {
         id: 1,
-        text: "2013 Scion Motivate Panelist & Mentor"
+        text: "2013 Scion Motivate Panelist & Mentor",
+        details: "Ran five workshops for dozens of up and coming small business owners..."
     },
     {
         id: 2,
-        text: "2019 Lawrence Foundation Award"
+        text: "2019 Lawrence Foundation Award",
+        details: "Awarded to Lawrence businesses who grow their staffing by 10% or more in the calendar year..."
     },
     {
         id: 3,
-        text: "2021 Kansas SBDC E3 Small Business Award"
+        text: "2021 Kansas SBDC E3 Small Business Award",
+        details: "Selected from over a thousand nominees..."
     }
 ];
