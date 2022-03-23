3d5b060d2c0a1e93845d8ea11df04a21f6cd2edc
Burton Parker <burtonparker@gmail.com>
2022-03-23 11:59:07 -0500

    formatting

--- portfolio-01/src/components/WorkComponent.js
+++ portfolio-01/src/components/WorkComponent.js
@@ -4,15 +4,17 @@ class Work extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="col-md-8">
-                    <h1>
-                        Selected Works
-                    </h1>
-                </div>
-                <div className="col-md-4">
-                    <p>
-                    Sed tempus urna et pharetra pharetra massa. Sit amet porttitor eget dolor morbi non. Nullam eget felis eget nunc lobortis mattis aliquam. Odio morbi quis commodo odio aenean. Sed tempus urna et pharetra pharetra massa massa ultricies. Odio aenean sed adipiscing diam donec adipiscing tristique risus. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sociis natoque penatibus et magnis dis. Sed felis eget velit aliquet. Nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum. Morbi quis commodo odio aenean sed adipiscing diam. Vel quam elementum pulvinar etiam non. Blandit volutpat maecenas volutpat blandit aliquam etiam. Sed blandit libero volutpat sed cras ornare. Et malesuada fames ac turpis egestas maecenas pharetra convallis. Vel fringilla est ullamcorper eget nulla. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus in.
-                    </p>
+                <div className="container mt-3">
+                    <div className="col-md-8">
+                        <h1>
+                            Selected Works
+                        </h1>
+                    </div>
+                    <div className="col-md-4">
+                        <p>
+                        Sed tempus urna et pharetra pharetra massa. Sit amet porttitor eget dolor morbi non. Nullam eget felis eget nunc lobortis mattis aliquam. Odio morbi quis commodo odio aenean. Sed tempus urna et pharetra pharetra massa massa ultricies. Odio aenean sed adipiscing diam donec adipiscing tristique risus. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sociis natoque penatibus et magnis dis. Sed felis eget velit aliquet. Nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum. Morbi quis commodo odio aenean sed adipiscing diam. Vel quam elementum pulvinar etiam non. Blandit volutpat maecenas volutpat blandit aliquam etiam. Sed blandit libero volutpat sed cras ornare. Et malesuada fames ac turpis egestas maecenas pharetra convallis. Vel fringilla est ullamcorper eget nulla. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus in.
+                        </p>
+                    </div>
                 </div>
             </React.Fragment>
         );
