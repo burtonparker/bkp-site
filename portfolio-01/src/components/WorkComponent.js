96ea843ff795736b0cfa4bc6cc2f8d0ecac7c6bb
Burton Parker <burtonparker@gmail.com>
2022-03-24 13:21:29 -0500

    more work

--- portfolio-01/src/components/WorkComponent.js
+++ portfolio-01/src/components/WorkComponent.js
@@ -93,6 +93,81 @@ class Work extends Component {
                             </Button>
                             </CardBody>
                         </Card>
+                        <Card>
+                            <CardImg
+                            alt="Kenyan Birth Certificate Generator"
+                            src="./assets/images/work-kenyan-birth-certificate-generator.jpg"
+                            top
+                            width="100%"
+                            />
+                            <CardBody>
+                            <CardTitle tag="h5">
+                                Kenyan Birth Certificate Generator
+                            </CardTitle>
+                            <CardSubtitle
+                                className="mb-2 text-muted"
+                                tag="h6"
+                            >
+                                Concept • Graphic Design
+                            </CardSubtitle>
+                            <CardText>
+                                About that time I accidentally went viral and made a bunch of people laugh (some people, not so much).
+                            </CardText>
+                            <Button>
+                                Learn More
+                            </Button>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardImg
+                            alt="Product Design"
+                            src="./assets/images/work-dillon-francis.jpg"
+                            top
+                            width="100%"
+                            />
+                            <CardBody>
+                            <CardTitle tag="h5">
+                                Various Clients
+                            </CardTitle>
+                            <CardSubtitle
+                                className="mb-2 text-muted"
+                                tag="h6"
+                            >
+                                Product Design • Merchandising
+                            </CardSubtitle>
+                            <CardText>
+                                In my role as merchandiser, I was responsible for designing products for hundreds of artists, comedians, and record labels over the years. Here's just a small sampling of that work.
+                            </CardText>
+                            <Button>
+                                Learn More
+                            </Button>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardImg
+                            alt="Vinyl Mailers"
+                            src="./assets/images/work-packaging-design.jpg"
+                            top
+                            width="100%"
+                            />
+                            <CardBody>
+                            <CardTitle tag="h5">
+                                Vinyl Mailers
+                            </CardTitle>
+                            <CardSubtitle
+                                className="mb-2 text-muted"
+                                tag="h6"
+                            >
+                                Packaging Design
+                            </CardSubtitle>
+                            <CardText>
+                                When you ship 40,000 vinyl albums each year, it's <i>pretty</i> important that they arrive safe and sound, so I designed a custom vinyl mailer and had it produced locally, helping the environment, and the local economy.
+                            </CardText>
+                            <Button>
+                                Learn More
+                            </Button>
+                            </CardBody>
+                        </Card>
                         </CardGroup>
                     </div>
                 </div>
