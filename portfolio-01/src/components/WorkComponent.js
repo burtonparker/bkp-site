ef5fe304db728be518886f980686284becea9fab
Burton Parker <burtonparker@gmail.com>
2022-03-24 11:03:34 -0500

    work - female collective, part 1

--- portfolio-01/src/components/WorkComponent.js
+++ portfolio-01/src/components/WorkComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardGroup, CardBody, CardSubtitle, CardText, Button, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 class Work extends Component {
     render() {
@@ -10,10 +11,89 @@ class Work extends Component {
                             Selected Works
                         </h1>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col">
                         <p>
-                        Sed tempus urna et pharetra pharetra massa. Sit amet porttitor eget dolor morbi non. Nullam eget felis eget nunc lobortis mattis aliquam. Odio morbi quis commodo odio aenean. Sed tempus urna et pharetra pharetra massa massa ultricies. Odio aenean sed adipiscing diam donec adipiscing tristique risus. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sociis natoque penatibus et magnis dis. Sed felis eget velit aliquet. Nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum. Morbi quis commodo odio aenean sed adipiscing diam. Vel quam elementum pulvinar etiam non. Blandit volutpat maecenas volutpat blandit aliquam etiam. Sed blandit libero volutpat sed cras ornare. Et malesuada fames ac turpis egestas maecenas pharetra convallis. Vel fringilla est ullamcorper eget nulla. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus in.
+                        Admission is free, so won't you please take a tour of some of my work?
                         </p>
+                    </div>
+                    <div className="col">
+                        <CardGroup>
+                        <Card>
+                            <CardImg
+                            alt="Female Collective"
+                            src="./assets/images/work-female-collective.jpg"
+                            top
+                            width="100%"
+                            />
+                            <CardBody>
+                            <CardTitle tag="h5">
+                                Female Collective
+                            </CardTitle>
+                            <CardSubtitle
+                                className="mb-2 text-muted"
+                                tag="h6"
+                            >
+                                UI • UX • Marketing
+                            </CardSubtitle>
+                            <CardText>
+                                I was responsible for translating Candace Reels dynamic voice into a shopping experience that felt like a natural extension of her social media presence.
+                            </CardText>
+                            <Button>
+                                Learn More
+                            </Button>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardImg
+                            alt="Card image cap"
+                            src="https://picsum.photos/318/180"
+                            top
+                            width="100%"
+                            />
+                            <CardBody>
+                            <CardTitle tag="h5">
+                                Card title
+                            </CardTitle>
+                            <CardSubtitle
+                                className="mb-2 text-muted"
+                                tag="h6"
+                            >
+                                Card subtitle
+                            </CardSubtitle>
+                            <CardText>
+                                This card has supporting text below as a natural lead-in to additional content.
+                            </CardText>
+                            <Button>
+                                Button
+                            </Button>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardImg
+                            alt="Card image cap"
+                            src="https://picsum.photos/318/180"
+                            top
+                            width="100%"
+                            />
+                            <CardBody>
+                            <CardTitle tag="h5">
+                                Card title
+                            </CardTitle>
+                            <CardSubtitle
+                                className="mb-2 text-muted"
+                                tag="h6"
+                            >
+                                Card subtitle
+                            </CardSubtitle>
+                            <CardText>
+                                This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+                            </CardText>
+                            <Button>
+                                Button
+                            </Button>
+                            </CardBody>
+                        </Card>
+                        </CardGroup>
                     </div>
                 </div>
             </React.Fragment>
